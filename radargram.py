07f34f344f5e6c70b2c7259ca16064bdc2ded70b
li.bo <bo.li@synsense.ai>
2023-07-27 17:31:48 +0800

    radargram

--- radargram.py
+++ radargram.py
@@ -1,129 +1,82 @@
 # -*- coding: utf-8 -*-
 # @Time    : 2023/7/26 21:43
 # @Author  : Li Bo
-# @FileName: leidatu.py
+# @FileName: radargram.py
 # @Software: PyCharm
 
-# import pandas as pd
-# import numpy as np
-# from sklearn.preprocessing import MinMaxScaler, StandardScaler
-# import matplotlib.pyplot as plt
-# # 假设有多个算法的性能数据，其中自己的算法优于其他算法
-# with plt.style.context(['science', 'grid']):
-#     algorithm_names = ['SF', 'EOG', 'MSMFM', 'EOL', 'SML', 'SML+EOL']
-#     performance_data = {
-#         'SSIM': [0.8415, 0.8410, 0.8416, 0.8395, 0.8421, 0.8417],  # 各个算法在Metric1上的性能数据
-#         'FMI': [0.5960, 0.6029, 0.5953, 0.5581, 0.5968, 0.5990],  # 各个算法在Metric2上的性能数据
-#         'Qabf': [0.7163, 0.6430, 0.7550, 0.7511, 0.7328, 0.7619],  # 各个算法在Metric2上的性能数据
-#         '$Q_{MI}$': [1.1707, 1.1503, 1.1668, 1.1333, 1.1707, 1.1716],  # 各个算法在Metric2上的性能数据
-#         '$Q_{S}$': [0.9148, 0.8826, 0.9425, 0.9437, 0.9287, 0.9458],  # 各个算法在Metric2上的性能数据
-#         '$Q_{CB}$': [0.7661, 0.7552, 0.7979, 0.7960, 0.7877, 0.8107],  # 各个算法在Metric2上的性能数据
-#         '$Q_G$': [0.6751, 0.6498, 0.7060, 0.7087, 0.6914, 0.7256],  # 各个算法在Metric2上的性能数据
-#         'Nabf': [0.0001, 0.0001, 0.0002, 0.0070, 0.0003, 0.0002],  # 各个算法在Metric2上的性能数据
-#
-#     }
-#
-#     # 创建数据框
-#     df = pd.DataFrame(performance_data, index=algorithm_names)
-#
-#     # 归一化处理
-#     scaler = MinMaxScaler(feature_range=(0.1, 1))
-#     df_normalized = pd.DataFrame(scaler.fit_transform(df), columns=df.columns, index=df.index)
-#
-#     # 计算雷达图中的角度
-#     angles = np.linspace(0, 2 * np.pi, num=8, endpoint=False).tolist()
-#     angles += angles[:1]  # 闭合雷达图
-#
-#     # 创建子图和坐标轴
-#     fig, ax = plt.subplots(figsize=(8, 8), subplot_kw=dict(polar=True))
-#
-#     # colors = ['#4C72B0', '#DD8452', '#55A868', '#8172B3']  # 设置线段的颜色
-#     # linewidths = [2, 2, 2, 2]  # 设置线段的粗细
-#     # 绘制每个算法的雷达图
-#     for i in range(len(algorithm_names)):
-#         values = df_normalized.iloc[i].tolist()
-#         values += values[:1]  # 闭合多边形
-#         ax.plot(angles, values, label=algorithm_names[i], linewidth=2)
-#         ax.fill(angles, values, alpha=0.2)  # 设置填充颜色
-#
-#     # 设置刻度标签
-#     ax.set_xticks(angles[:-1])
-#     ax.set_xticklabels(df.columns)
-#
-#     # 添加网格线
-#     ax.yaxis.grid(True)
-#
-#     # 添加图例
-#     plt.legend(loc='upper right', bbox_to_anchor=(0.1, 0.1))
-#
-#     # 添加标题
-#     # plt.title('Normalized Performance Comparison of Algorithms')
-#     plt.tight_layout()
-#     # 显示图形
-#     plt.savefig("1111.pdf", dpi=300, format="pdf", bbox_inches="tight")
-#     # 显示图表
-#     plt.show()
-
+from typing import List
 
 import matplotlib.pyplot as plt
-import pandas as pd
 import numpy as np
-from sklearn.preprocessing import MinMaxScaler
 
-# Rest of your code (omitted for brevity)
-with plt.style.context(['science', 'grid']):
-    algorithm_names = ['SF', 'EOG', 'MSMFM', 'EOL', 'SML', 'SML+EOL']
-    performance_data = {
-        'SSIM': [0.8415, 0.8410, 0.8416, 0.8395, 0.8421, 0.8417],  # 各个算法在Metric1上的性能数据
-        'FMI': [0.5960, 0.6029, 0.5953, 0.5581, 0.5968, 0.5990],  # 各个算法在Metric2上的性能数据
-        'Qabf': [0.7163, 0.6430, 0.7550, 0.7511, 0.7328, 0.7619],  # 各个算法在Metric2上的性能数据
-        '$Q_{MI}$': [1.1707, 1.1503, 1.1668, 1.1333, 1.1707, 1.1716],  # 各个算法在Metric2上的性能数据
-        '$Q_{S}$': [0.9148, 0.8826, 0.9425, 0.9437, 0.9287, 0.9458],  # 各个算法在Metric2上的性能数据
-        '$Q_{CB}$': [0.7661, 0.7552, 0.7979, 0.7960, 0.7877, 0.8107],  # 各个算法在Metric2上的性能数据
-        '$Q_G$': [0.6751, 0.6498, 0.7060, 0.7087, 0.6914, 0.7256],  # 各个算法在Metric2上的性能数据
-        'Nabf': [0.0001, 0.0001, 0.0002, 0.0070, 0.0003, 0.0002],  # 各个算法在Metric2上的性能数据
 
-    }
+algorithm_names = ['SF', 'EOG', 'MSMFM', 'EOL', 'SML', 'SML+EOL']
+results = [{"SSIM": 87, "FMI": 79, "Qabf": 95, "$Q_{MI}$": 92, "$Q_{S}$": 85, "$Q_{CB}$": 86, "$Q_G$": 86},
+           {"大学英语": 80, "高等数学": 90, "体育": 91, "计算机基础": 85, "程序设计": 88, "程序": 87, "序": 86}]
+data_length = len(results[0])
+angles = np.linspace(0, 2 * np.pi, data_length, endpoint=False)
+labels = [key for key in results[0].keys()]
+score = [[v for v in result.values()] for result in results]
 
-    # 创建数据框
-    df = pd.DataFrame(performance_data, index=algorithm_names)
+score_a = [[0.8153846153846303, 0.8767857142857132, 0.6931875525651812, 0.9812010443864239, 0.6075949367088587,
+            0.3571171171171166, 0.4670184696569919, 0.8153846153846303],
+           [0.6615384615384414, 1.0, 0.20000000000000018, 0.5550913838120124, 0.1999999999999993, 0.1999999999999993,
+            0.20000000000000018, 0.6615384615384414],
+           [0.846153846153868, 0.8642857142857157, 0.9535744322960475, 0.8997389033942582, 0.9582278481012647,
+            0.8154954954954956, 0.7931398416886539, 0.846153846153868],
+           [0.19999999999998863, 0.1999999999999993, 0.927333894028596, 0.1999999999999993, 0.9734177215189863,
+            0.7881081081081085, 0.8216358839050129, 0.19999999999998863],
+           [1.0, 0.8910714285714292, 0.804205214465938, 0.9812010443864239, 0.7835443037974663, 0.6684684684684683,
+            0.6390501319261208, 1.0],
+           [0.8769230769231058, 0.930357142857142, 1.0000000000000009, 1.0, 0.9999999999999982, 1.0, 1.0,
+            0.8769230769231058]]
 
-    # 归一化处理
-    scaler = MinMaxScaler(feature_range=(0.1, 1))
-    df_normalized = pd.DataFrame(scaler.fit_transform(df), columns=df.columns, index=df.index)
+score_b = [[0.9737226277372244, 0.9880553532410778, 0.7159678858162364, 0.888734602463606, 0.1999999999999993,
+            0.8509539842873179, 0.7877073170731708, 0.9737226277372244],
+           [0.991240875912407, 0.9822286962855062, 0.5853702051739527, 1.0, 0.4520900321543415, 0.8716049382716058,
+            0.8907317073170733, 0.991240875912407],
+           [0.9737226277372244, 0.9927166788055355, 1.0000000000000009, 0.9007390817469205, 0.9948553054662383,
+            0.8967452300785643, 0.9711219512195124, 0.9737226277372244],
+           [0.1999999999999993, 0.19999999999999996, 0.20000000000000018, 0.19999999999999996, 0.23858520900321523,
+            0.20000000000000018, 0.19999999999999996, 0.1999999999999993],
+           [0.9883211678832069, 0.9985433357611071, 0.838001784121321, 0.9163269876819711, 0.7736334405144696,
+            0.8985409652076326, 0.9438048780487807, 0.9883211678832069],
+           [1.0, 1.0, 0.9743086529884044, 0.9174020156774918, 1.0, 1.0, 1.0000000000000002, 1.0]]
+angles = np.concatenate((angles, [angles[0]]))
+labels = np.concatenate((labels, [labels[0]]))
+fig = plt.figure(figsize=(10, 6))
+# fig.suptitle("计算机专业大一（上）")
+ax1 = plt.subplot(121, polar=True)
+ax2 = plt.subplot(122, polar=True)
+ax, data, name = [ax1, ax2], [score_a, score_b], ["Dataset: Lytro", "Dataset: MFFW"]
+for i in range(2):
+    for j in np.arange(0, 1 + 0.2, 0.2):
+        ax[i].plot(angles, 8 * [j], '--', lw=0.5, color='black')
+    for j in range(7):
+        ax[i].plot([angles[j], angles[j]], [0, 1], '-', lw=0.5, color='gray')
+    for num in range(6):
+        ax[i].plot(angles, data[i][num], lw=1, label=algorithm_names[num])
+        # 隐藏最外圈的圆
+        ax[i].spines['polar'].set_visible(False)
+        # 隐藏圆形网格线
+        ax[i].grid(False)
+        # for a, b in zip(angles, data[i]):
+        #     ax[i].text(a, b+5, '%.00f' % b, ha='center', va='center', fontsize=12, color='b')
+        ax[i].fill(angles, data[i][num], alpha=0.2)  # 设置填充颜色
+        ax[i].set_thetagrids(angles * 180 / np.pi, labels)
+        ax[i].set_theta_zero_location('N')
+        ax[i].set_rlim(0, 1)
+        ax[i].set_rlabel_position(0)
 
-    # 计算雷达图中的角度
-    angles = np.linspace(0, 2 * np.pi, num=8, endpoint=False).tolist()
-    angles += angles[:1]  # 闭合雷达图
-    # Create two radar charts side by side
-    fig, axs = plt.subplots(1, 2, figsize=(16, 8), subplot_kw=dict(polar=True))
+        leg = ax[i].legend()
+        # legend = ax[i].legend()
+        # legend.set_facecolor('None')
 
-    # Loop through each subplot (axis) and plot the radar chart
-    for ax in axs:
-        # Rest of your code for radar chart (omitted for brevity)
+        # 设置图例里线段的宽度
+        for legobj in ax[i].legend().legendHandles:
+            legobj.set_linewidth(5)
 
-        # Plot each algorithm's radar chart on the corresponding axis
-        for i in range(len(algorithm_names)):
-            values = df_normalized.iloc[i].tolist()
-            values += values[:1]  # 闭合多边形
-            ax.plot(angles, values, label=algorithm_names[i], linewidth=2)
-            ax.fill(angles, values, alpha=0.2)  # 设置填充颜色
-            ax.set_xticks(angles[:-1])
-            ax.set_xticklabels(df.columns)
-        # Rest of your code for radar chart (omitted for brevity)
+        ax[i].set_title(name[i])
 
-    # Add titles to each subplot
-    axs[0].set_title('Dataset: Lytro')
-    axs[1].set_title('Dataset: MFFW')
-
-    # Show the legend outside of the plots
-    # axs[0].legend(loc='upper right', bbox_to_anchor=(0.1, 0.1))
-    axs[1].legend(loc='lower right', bbox_to_anchor=(0.01, 0.1))
-    # Adjust layout and spacing
-    plt.tight_layout()
-
-    # Save the figure to a file
-    # plt.savefig("radar_charts.pdf", dpi=300, format="pdf", bbox_inches="tight")
-
-    # Show the plot
-    plt.show()
+plt.savefig("radar_charts.pdf", dpi=300, format="pdf", bbox_inches="tight")
+plt.show()
