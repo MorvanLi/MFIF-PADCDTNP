934b5268836ac26c837e0219c0824e7fe3df1619
li.bo <bo.li@synsense.ai>
2023-08-03 16:03:20 +0800

    scnn

---
+++ scnn/__init__.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+"""
+@Author  : Morvan Li
+@FileName: __init__.py.py
+@Software: PyCharm
+@Time    : 8/3/23 3:15 PM
+"""
+from .utils import get_cfg, setup_logger
+
+# 启动logger配置
+setup_logger()
